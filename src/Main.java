85f641079e1092d0caa98d2bd3893442b51ab810
danielpoinstingl <daniel.poinstingl@gmx.at>
2025-02-25 12:44:55 +0100

    basisklasse main first commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Neu");
+        System.out.println("Das gibt einen Konflikt");
     }
 }
