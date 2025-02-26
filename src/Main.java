f59d46ae06387af2ad9773d852f432eced018734
arlind <hysajarlind@yahoo.com>
2025-02-26 08:19:55 +0100

    QuadratischeBasis.java

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Kegel kegel=new Kegel(7,3);
         System.out.println(kegel.calcvolumen());
+
+        QuadratischeBasis qb=new QuadratischeBasis(3,10);
+        System.out.println(qb.calcvolumen());
     }
 }
--- src/QuadratischeBasis.java
+++ src/QuadratischeBasis.java
@@ -1,15 +1,19 @@
-public class QuadratischeBasis {
+public class QuadratischeBasis extends Pyramide {
     private int seite;
     private int triangleH;
 
     public QuadratischeBasis(int seite, int triangleH) {
+        super((float)Math.sqrt(triangleH * triangleH - (seite / 2.0) * (seite / 2.0)));
         this.seite = seite;
         this.triangleH = triangleH;
+
     }
 
+
     @Override
-    public int calcHoehe(seite, triangleH){
-    double h = Math.sqrt(s * s - (a / 2.0) * (a / 2.0));
+    public float calcvolumen(){
+    float V = (float) ((seite * seite * getHight()) / 3.0);
+    return  V;
 
     }
 }
